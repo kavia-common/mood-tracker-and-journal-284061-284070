0303ba75c0cd1389e048cd15bcd549d72880c744
Kavia AI <production@kavia.ai>
2025-12-01 05:28:04 +0000

    Checkpoint 3 - cgb68887da

---
+++ mood_log_database/database_backup.sql
@@ -0,0 +1,170 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict Ou9SbPj3kBl1xaavPerYMQPDiqxYfSE7zr42zwXN1x1ppjTF1FfEfLjT4eWZOlA
+
+-- Dumped from database version 16.10 (Ubuntu 16.10-0ubuntu0.24.04.1)
+-- Dumped by pg_dump version 16.10 (Ubuntu 16.10-0ubuntu0.24.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE IF EXISTS myapp;
+--
+-- Name: myapp; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE myapp WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.UTF-8';
+
+
+ALTER DATABASE myapp OWNER TO postgres;
+
+\unrestrict Ou9SbPj3kBl1xaavPerYMQPDiqxYfSE7zr42zwXN1x1ppjTF1FfEfLjT4eWZOlA
+\connect myapp
+\restrict Ou9SbPj3kBl1xaavPerYMQPDiqxYfSE7zr42zwXN1x1ppjTF1FfEfLjT4eWZOlA
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: appuser
+--
+
+CREATE TABLE public.schema_migrations (
+    id integer NOT NULL,
+    filename text NOT NULL,
+    applied_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE public.schema_migrations OWNER TO appuser;
+
+--
+-- Name: schema_migrations_id_seq; Type: SEQUENCE; Schema: public; Owner: appuser
+--
+
+CREATE SEQUENCE public.schema_migrations_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.schema_migrations_id_seq OWNER TO appuser;
+
+--
+-- Name: schema_migrations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: appuser
+--
+
+ALTER SEQUENCE public.schema_migrations_id_seq OWNED BY public.schema_migrations.id;
+
+
+--
+-- Name: schema_migrations id; Type: DEFAULT; Schema: public; Owner: appuser
+--
+
+ALTER TABLE ONLY public.schema_migrations ALTER COLUMN id SET DEFAULT nextval('public.schema_migrations_id_seq'::regclass);
+
+
+--
+-- Data for Name: schema_migrations; Type: TABLE DATA; Schema: public; Owner: appuser
+--
+
+COPY public.schema_migrations (id, filename, applied_at) FROM stdin;
+\.
+
+
+--
+-- Name: schema_migrations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: appuser
+--
+
+SELECT pg_catalog.setval('public.schema_migrations_id_seq', 1, false);
+
+
+--
+-- Name: schema_migrations schema_migrations_filename_key; Type: CONSTRAINT; Schema: public; Owner: appuser
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_filename_key UNIQUE (filename);
+
+
+--
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: appuser
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: DATABASE myapp; Type: ACL; Schema: -; Owner: postgres
+--
+
+GRANT ALL ON DATABASE myapp TO appuser;
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: pg_database_owner
+--
+
+GRANT ALL ON SCHEMA public TO appuser;
+
+
+--
+-- Name: DEFAULT PRIVILEGES FOR SEQUENCES; Type: DEFAULT ACL; Schema: public; Owner: postgres
+--
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON SEQUENCES TO appuser;
+
+
+--
+-- Name: DEFAULT PRIVILEGES FOR TYPES; Type: DEFAULT ACL; Schema: public; Owner: postgres
+--
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TYPES TO appuser;
+
+
+--
+-- Name: DEFAULT PRIVILEGES FOR FUNCTIONS; Type: DEFAULT ACL; Schema: public; Owner: postgres
+--
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON FUNCTIONS TO appuser;
+
+
+--
+-- Name: DEFAULT PRIVILEGES FOR TABLES; Type: DEFAULT ACL; Schema: public; Owner: postgres
+--
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TABLES TO appuser;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict Ou9SbPj3kBl1xaavPerYMQPDiqxYfSE7zr42zwXN1x1ppjTF1FfEfLjT4eWZOlA
+
